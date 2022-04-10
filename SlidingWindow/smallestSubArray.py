0c6fa6ccc88e177ec33d09c58f75b165b653902e
Alhassan Hassan <66450402+alhassan-hassan@users.noreply.github.com>
2022-04-10 13:09:08 +0000

    mergedList

---
+++ SlidingWindow/smallestSubArray.py
@@ -0,0 +1,6 @@
+def smallestSubArray(arr, target):
+    #  We should have a window end that takes us to the end of the array.
+    # Also, we will need a winsdow start that only increses if we want to shrink the subarray
+
+
+print(smallestSubArray([4,2,2,7,8,1,2,8,10]), 10)
