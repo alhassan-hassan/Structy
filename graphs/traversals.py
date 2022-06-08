876f1f77b37698aa5c5d314a90ab061f69b0e5c1
Alhassan Hassan <66450402+alhassan-hassan@users.noreply.github.com>
2022-06-08 07:31:17 +0000

    graphs

--- graphs/traversals.py
+++ graphs/traversals.py
@@ -1,3 +1,4 @@
+from collections import deque
 graph = {
     "a": ["b", "c"],
     "b": ["d"],
@@ -7,6 +8,8 @@ graph = {
     "f": []
 }
 
+# 1. DFS
+# ITERATIVELY
 def dfs(graph, start):
     stack = [start]
     
@@ -16,4 +19,25 @@ def dfs(graph, start):
         for neighbor in graph[cur]:
             stack.append(neighbor)
         
-dfs(graph, "a")
+# dfs(graph, "a")
+
+# RECURSIVELY
+def dfs_recur(graph, start):
+    print(start)
+    for neighbor in graph[start]:
+        dfs_recur(graph, neighbor)
+        
+# dfs_recur(graph, "a")
+
+
+# 2. BFS
+def bfs(graph, current):
+    dq = deque([current])
+    while dq:
+        cur = dq.popleft()
+        print(cur)
+        for neighbor in graph[cur]:
+            dq.append(neighbor)
+            
+bfs(graph, "a")
+    
