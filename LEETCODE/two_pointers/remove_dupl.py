b6475e581fad022b8a7109093fcffbe94f065a81
Alhassan Hassan <66450402+alhassan-hassan@users.noreply.github.com>
2022-07-27 03:43:45 +0000

    pointers

--- LEETCODE/two_pointers/remove_dupl.py
+++ LEETCODE/two_pointers/remove_dupl.py
@@ -1,5 +1,73 @@
 class Solution:
-    def removeDuplicates(self, nums) -> int:
+    def removeDuplicates(self, nums):
+        """
+        [0, 0 , 1 , 1 , 1 , 2 , 2 , 3 , 3 , 4]
+         *
+                *
+        """
+        
+#         if len(nums) < 2:
+#             return len(nums)
+        
+#         sl, fs, tr = 0, 1, 1
+        
+#         while fs < len(nums):
+#             if nums[sl] == nums[fs]:
+#                 fs += 1
+#             else:
+#                 nums[tr] = nums[fs]
+#                 tr += 1
+#                 sl = fs
+#                 fs += 1
+#         return tr
+        
+        if len(nums) < 2:
+            return len(nums)
+        
+        track = 0
+        
+        for i in range(1, len(nums)):
+            if nums[track] != nums[i]:
+                track += 1
+                nums[track] = nums[i]
+        return track + 1
+                
+    
+    
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         # THEIR APPROACH
 #         if len(nums) == 0:
@@ -16,6 +84,9 @@ class Solution:
     
     # MY APPROACH - BEST
     
+       
+        
+     
         sl = 0
         fs = 1
         track = 1
@@ -33,41 +104,32 @@ class Solution:
                 fs += 1
                 
         return track
-
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     
         
-#         if len(nums) == 0:
-#             return []
+        if len(nums) == 0:
+            return []
         
-#         ind = 0
-#         slow = 0
-#         fast = 0
+        ind = 0
+        slow = 0
+        fast = 0
         
-#         while fast < len(nums):
-#             if fast == 0:
-#                 nums[ind] = nums[slow]
-#                 ind += 1
-#                 fast += 1
-#             else:
-#                 if nums[slow] == nums[fast]:
-#                     fast += 1
-#                 else:
-#                     nums[ind] = nums[fast]
-#                     slow = fast
-#                     fast += 1
-#                     ind += 1
+        while fast < len(nums):
+            if fast == 0:
+                nums[ind] = nums[slow]
+                ind += 1
+                fast += 1
+            else:
+                if nums[slow] == nums[fast]:
+                    fast += 1
+                else:
+                    nums[ind] = nums[fast]
+                    slow = fast
+                    fast += 1
+                    ind += 1
                     
-#         nums[ : ] = nums[ 0 : ind]
+        nums[ : ] = nums[ 0 : ind]
                     
             
         
---
+++ LEETCODE/two_sums/two_sum_II.py
@@ -0,0 +1,33 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        """
+        [2, 7 , 1 1 , 15]
+         *
+                      *
+        summ greater, reduce fast pointer
+        summ less, increase the right pointer
+        same, return the indices of the two numbers => return the two pointers
+        
+        Runtime: Time = O(n)
+                 Space = 0(1)
+                 
+        """
+        if len(nums) < 2:
+            return []
+        
+        sl = 0
+        fast = len(nums) - 1
+        res = []
+        
+        while sl < fast:
+            summ = nums[sl] + nums[fast]
+            if summ > target:
+                fast -= 1
+            elif summ < target:
+                sl += 1
+            else:
+                res.extend([sl+1, fast+1])
+                return res
+        return res
+                
