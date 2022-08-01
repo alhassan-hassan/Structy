a1acd6aa278f964dd6fa7957f049ad2efb4c6648
Alhassan Hassan <66450402+alhassan-hassan@users.noreply.github.com>
2022-08-01 22:01:29 +0000

    binary search

---
+++ LEETCODE/binary_Search/capacity_shipping.py
@@ -0,0 +1,25 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def isShippable(capacity):
+            dayCount, curCapacity = 1, 0
+            
+            for weight in weights:
+                if curCapacity + weight <= capacity:
+                    curCapacity += weight
+                else:
+                    dayCount += 1
+                    curCapacity = weight
+                    
+            return dayCount <= days
+        
+        minW, maxW = max(weights), sum(weights)
+        
+        while minW < maxW:
+            mid = (minW + maxW) // 2
+            
+            if isShippable(mid):
+                maxW = mid
+            else:
+                minW = mid + 1
+                
+        return minW
