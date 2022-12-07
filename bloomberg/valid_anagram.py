0c6e36261f82a1c7b99c77544753494c23716122
Hassan Alhassan <hassanalhassan@Hassans-MacBook-Pro.local>
2022-12-07 14:15:20 +0000

    VALID ANAGRAMS

---
+++ bloomberg/valid_anagram.py
@@ -0,0 +1,7 @@
+import collections
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count1 = collections.Counter(s)
+        count2 = collections.Counter(t)
+
+        return count1 == count2
