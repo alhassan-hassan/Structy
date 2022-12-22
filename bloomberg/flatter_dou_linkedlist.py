b86a7e3e32310e4c2bf9574c7107a627c3730e5e
alhassan-hassan <alhassanhassan360@gmail.com>
2022-12-22 03:14:01 +0000

    letss go

--- bloomberg/flatter_dou_linkedlist.py
+++ bloomberg/flatter_dou_linkedlist.py
@@ -8,7 +8,57 @@ class Node:
         self.child = child
 
 class Solution:
-    def flatten(self, head):
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return head
+
+        pseudoHead = Node(0, None, head, None)
+        prev = pseudoHead
+        stack = [head]
+
+        while stack:
+            cur = stack.pop()
+            prev.next = cur
+            cur.prev = prev
+            prev = cur
+
+            if cur.next:
+                stack.append(cur.next)
+            
+            if cur.child:
+                stack.append(cur.child)
+                cur.child = None
+            
+            
+        pseudoHead.next.prev = None
+
+        return pseudoHead.next
+
+
+
+        # if not head:
+        #     return head
+
+        # cur = head
+        # stack = []
+        # res = []
+
+        # while cur is not None or len(stack) > 0:
+        #     if cur is None:
+        #         cur = stack.pop().next
+        #     else:
+        #         res.append(cur.val)
+        #         if cur.child:
+        #             stack.append(cur)
+        #             cur = cur.child
+        #         else:
+        #             cur = cur.next
+
+        # return Node(",".join(str(x) for x in res))
+
+
+
+        
         if not head:
             return None
         
@@ -21,8 +71,7 @@ class Solution:
             
             if cur is None:
                 cur = stack.pop().next
-            
-            if cur is not None:
+            else:
                 res.append(cur.val)
                 if cur.child is not None:
                     stack.append(cur)
