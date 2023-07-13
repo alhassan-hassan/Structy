d5b1a4f5a8862b229eba3fcae03315be4293c339
alhassan-hassan <alhassanhassan360@gmail.com>
2023-07-12 23:35:33 -0700

    evaluate division

---
+++ blackrock/evaluate_division.py
@@ -0,0 +1,75 @@
+import collections
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = collections.defaultdict(dict)
+        N = len(equations)
+
+        for i in range(N):
+            graph[equations[i][0]][equations[i][1]] = values[i]
+            graph[equations[i][1]][equations[i][0]] = 1 / values[i]
+
+        def dfs(x, y, visited):
+            if x not in graph or y not in graph:
+                return -1
+            
+            if y in graph[x]:
+                return graph[x][y]
+
+            for component in graph[x]:
+                if component not in visited:
+                    visited.add(component)
+                    tempt = dfs(component, y, visited)
+                    if tempt == -1:
+                        continue
+                    else:
+                        return tempt * graph[x][component]
+            return -1
+
+        output = []
+
+        for p,q in queries:
+            output.append(dfs(p, q, set()))
+
+        return output
+
+
+
+# class Solution:
+#     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+#         bodmas = collections.defaultdict(dict)
+#         res = []
+        
+#         for i in range(len(equations)):
+#             first, second = equations[i]
+#             bodmas[first][second] = values[i]
+#             bodmas[second][first] = 1 / values[i]
+
+#         def dfs(query, val, visited):
+#             x, y = query
+#             if x not in bodmas or y not in bodmas:
+#                 res.append(-1.0)
+#                 return 
+
+#             curBod = bodmas[x]
+#             if y in curBod:
+#                 val *= curBod[y]
+#                 res.append(val)
+#                 return
+                
+#             visited.add(x)
+#             for other in curBod:
+#                 if other not in visited:
+#                     val *= bodmas[x][other]
+#                     nQuery = [other, y]
+#                     dfs(nQuery, val, visited)
+
+#         for num in queries:
+#             dfs(num, 1, set())
+
+#         return res
+
+
+
+
+
+        
