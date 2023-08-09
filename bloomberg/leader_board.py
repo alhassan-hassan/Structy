6094d3c04bb4409334506b194bdb8bf6c10d4cc7
alhassan-hassan <alhassanhassan360@gmail.com>
2023-08-09 04:20:24 +0000

    improved

--- bloomberg/leader_board.py
+++ bloomberg/leader_board.py
@@ -27,3 +27,60 @@ class Leaderboard:
     def reset(self, playerId: int) -> None:
         del self.board[playerId]
         
+
+from sortedcontainers import SortedDict
+class Leaderboard:
+
+    def __init__(self):
+        self.board = {}
+        self.sortedScores = SortedDict()
+        
+    def addScore(self, playerId: int, score: int) -> None:
+        if playerId not in self.board:
+            self.board[playerId] = score
+            self.sortedScores[-score] = self.sortedScores.get(-score, 0) + 1
+        else:
+            oldVal = self.board[playerId]
+            val = self.sortedScores.get(-oldVal)
+
+            if val == 1:
+                del self.sortedScores[-oldVal]
+            else:
+                self.sortedScores[-oldVal] = val - 1
+
+            newVal = oldVal + score
+            self.board[playerId] = newVal
+            self.sortedScores[-newVal] = self.sortedScores.get(-newVal, 0) + 1
+
+    def top(self, K: int) -> int:
+        count = total = 0
+
+        for key, val in self.sortedScores.items():
+            for i in range(val):
+                total += key
+                count += 1
+
+                if count == K:
+                    break
+            if count == K:
+                break
+
+        return total * -1
+
+    def reset(self, playerId: int) -> None:
+        score = self.board[playerId]
+        if self.sortedScores[-score] == 1:
+            del self.sortedScores[-score]
+        else:
+            self.sortedScores[-score] -= 1
+
+        del self.board[playerId]
+
+        
+
+
+# Your Leaderboard object will be instantiated and called as such:
+# obj = Leaderboard()
+# obj.addScore(playerId,score)
+# param_2 = obj.top(K)
+# obj.reset(playerId)
