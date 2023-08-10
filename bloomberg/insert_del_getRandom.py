21d06d823a371bfbfdcfb3779ceb9ea9cb541996
alhassan-hassan <alhassanhassan360@gmail.com>
2023-08-10 08:58:06 +0000

    ird

--- bloomberg/insert_del_getRandom.py
+++ bloomberg/insert_del_getRandom.py
@@ -19,52 +19,38 @@ class RandomizedSet:
     def getRandom(self) -> int:
         return random.choice(list(self.set_))
 
-        from random import choice
+from random import choice
 
 
-        # OR THE IMPLEMENTATION BELOW WORLK BETTER ALSO
+# OR THE IMPLEMENTATION BELOW WORLK BETTER ALSO
 
 import random
-class RandomizedSet():
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.dict = {}
-        self.list = []
+import random
+class RandomizedSet:
 
+    def __init__(self):
+        self.ind = {}
+        self.cont = []
         
     def insert(self, val: int) -> bool:
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        """
-        if val in self.dict:
-            return False
-        self.dict[val] = len(self.list)
-        self.list.append(val)
-        return True
-        
-
-    def remove(self, val: int) -> bool:
-        """
-        Removes a value from the set. Returns true if the set contained the specified element.
-        """
-        if val in self.dict:
-            # move the last element to the place idx of the element to delete
-            last_element, idx = self.list[-1], self.dict[val]
-            self.list[idx], self.dict[last_element] = last_element, idx
-            # delete the last element
-            self.list.pop()
-            del self.dict[val]
+        if val not in self.ind:
+            self.ind[val] = len(self.cont)
+            self.cont.append(val)
             return True
         return False
 
+    def remove(self, val: int) -> bool:
+        if val in self.ind:
+            cur_ind, last_element = self.ind[val], self.cont[-1]
+            self.cont[cur_ind], self.ind[last_element] = last_element, cur_ind
+            self.cont.pop()
+            del self.ind[val]
+            return True
+        
+        return False
+        
     def getRandom(self) -> int:
-        """
-        Get a random element from the set.
-        """
-        return random.choice(self.list)
-
+        return random.choice(self.cont)
         
 
 
