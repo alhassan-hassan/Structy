23f137d3fb057b98f73dff076f5ad87575c7b8dd
alhassan-hassan <alhassanhassan360@gmail.com>
2023-08-13 07:26:53 +0000

    lets go for all the paths

--- bloomberg/all_paths_to_desti.py
+++ bloomberg/all_paths_to_desti.py
@@ -7,7 +7,6 @@ class Solution:
 
         def backtrack(cur_node, path):
             if cur_node == target:
-                print(path)
                 res.append(list(path))
                 return
 
@@ -21,5 +20,31 @@ class Solution:
         backtrack(begin, path)
 
         return res
+    
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        begin = 0
+        trg = len(graph) - 1
+        
+        res = []
+
+        def dfs(cur, path):
+            if cur == trg:
+                # create a copy of the list and append it to the result
+                res.append(list(path))
+                return
+
+            for neighbor in graph[cur]:
+                path.append(neighbor)
+                dfs(neighbor, path)
+                path.pop()
+
+        path = [0]
+        dfs(begin,path)
+
+        return res
+
+
 
         
