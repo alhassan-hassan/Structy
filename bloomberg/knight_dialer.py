de27bfedfc6e460785457cc79f68bb86a33d5c67
alhassan-hassan <alhassanhassan360@gmail.com>
2023-08-14 21:33:55 +0000

    knight dialer

--- bloomberg/knight_dialer.py
+++ bloomberg/knight_dialer.py
@@ -19,3 +19,25 @@ class Solution:
             calls[9] = old_calls[2] + old_calls[4]
 
         return sum(calls) % MOD
+    
+
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        calls = [1] * 10
+        MOD = 10**9 + 7
+
+        for i in range(1, n):
+            old_calls = calls.copy()
+
+            calls[0] = old_calls[6] + old_calls[4]
+            calls[1] = old_calls[8] + old_calls[6]
+            calls[2] = old_calls[7] + old_calls[9]
+            calls[3] = old_calls[8] + old_calls[4]
+            calls[4] = old_calls[3] + old_calls[9] + old_calls[0]
+            calls[5] = 0
+            calls[6] = old_calls[1] + old_calls[7] + old_calls[0]
+            calls[7] = old_calls[2] + old_calls[6]
+            calls[8] = old_calls[3] + old_calls[1]
+            calls[9] = old_calls[2] + old_calls[4]
+
+        return sum(calls) % MOD
