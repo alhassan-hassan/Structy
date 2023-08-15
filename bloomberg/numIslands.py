9fc406fb98506e62cb135bec45b63f8e34646ebb
alhassan-hassan <alhassanhassan360@gmail.com>
2023-08-15 11:54:06 +0000

    number of islands

--- bloomberg/numIslands.py
+++ bloomberg/numIslands.py
@@ -37,3 +37,43 @@ class Solution:
                     islands += 1
 
         return islands
+    
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        verts = defaultdict(list)
+        queue = deque([(root, 0)])
+
+        min_ = max_ = 0
+
+        while queue:
+            node, ind = queue.popleft()
+            if node is not None:
+                verts[ind].append(node.val)
+
+                queue.append((node.left, ind - 1))
+                queue.append((node.right, ind + 1))
+
+                min_ = min(min_, ind)
+                max_ = max(max_, ind)
+
+        output = []
+
+        for key in range(min_, max_ + 1):
+            output.append(verts[key])
+
+        return output
+
+"""
+COMPLEXITIES: TIME: O(N)
+              SPACE:O(N)
+"""
