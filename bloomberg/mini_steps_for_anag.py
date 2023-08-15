9077d2b78bfcb1ae6ec4e6f9247fb21c14439d3a
alhassan-hassan <alhassanhassan360@gmail.com>
2023-08-15 07:14:39 +0000

    minimum steps to make two strins anagrams

--- bloomberg/mini_steps_for_anag.py
+++ bloomberg/mini_steps_for_anag.py
@@ -7,3 +7,24 @@ class Solution:
             counts[ord(t[i]) - 97] -= 1
 
         return sum([x if x > 0 else 0 for x in counts])
+    
+
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        memo1 = defaultdict(int)
+
+        for char in s:
+            memo1[char] += 1
+
+        count = 0
+        for char in t:
+            if char in memo1 and memo1[char]:
+                memo1[char] -= 1
+
+        return sum(memo1.values())
+
+
+"""
+COMPLEXITIES: TIME : O(max(n, m))
+              SPACE: O(m)
+"""
