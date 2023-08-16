054e8c88b7ef59502cdb9774dd7f57fd1c4dd3b5
alhassan-hassan <alhassanhassan360@gmail.com>
2023-08-16 22:05:02 +0000

    lru

--- bloomberg/lru_cache.py
+++ bloomberg/lru_cache.py
@@ -20,8 +20,27 @@ class LRUCache:
             if len(self.ordered) > self.capacity:
                 self.ordered.popitem(last = False)
         
+git
+class LRUCache:
 
+    def __init__(self, capacity: int):
+        self.lru = collections.OrderedDict()
+        self.capacity = capacity
+        
+    def get(self, key: int) -> int:
+        if key in self.lru:
+            self.lru.move_to_end(key)
+            return self.lru[key]
+        return -1
+        
+    def put(self, key: int, value: int) -> None:
+        if key in self.lru:
+            self.lru.move_to_end(key)
 
+        self.lru[key] = value
+        if len(self.lru) > self.capacity:
+            self.lru.popitem(last = False)
+        
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
