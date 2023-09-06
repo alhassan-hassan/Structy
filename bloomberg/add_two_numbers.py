ca6275e82b9dc892c564070de769f48a1ad7ebf9
alhassan-hassan <alhassanhassan360@gmail.com>
2023-09-06 18:57:54 +0000

    add two num

--- bloomberg/add_two_numbers.py
+++ bloomberg/add_two_numbers.py
@@ -33,6 +33,74 @@ class Solution:
 
         # OPTION 2
         
+        def getNumbers(node):
+            nums = []
+            cur = node
+
+            while cur:
+                nums.append(cur.val)
+                cur = cur.next
+
+            return nums
+
+        list1 = getNumbers(l1)
+        list2 = getNumbers(l2)
+
+        carry = 0
+        head = None
+
+        while list1 or list2 or carry != 0:
+            sum_ = carry
+            if list1: sum_ += list1.pop()
+            if list2: sum_ += list2.pop()
+
+            newNode = ListNode(sum_ % 10)
+            newNode.next = head
+            head = newNode
+
+            carry = sum_ // 10
+
+        return head
+    
+        # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        def nums(head):     
+            cur = head
+            res = []
+
+            while cur:
+                res.append(cur.val)
+                cur = cur.next
+
+            return res
+
+        stack1 = nums(l1)
+        stack2 = nums(l2)
+
+        carry = 0
+        head = None
+
+        while stack1 or stack2 or carry != 0:
+            sum_ = carry
+
+            if stack1:
+                sum_ += stack1.pop()
+            
+            if stack2:
+                sum_ += stack2.pop()
+
+            newNode = ListNode(sum_ % 10)
+            newNode.next = head
+            head = newNode
+
+            carry = sum_ // 10
+
+        return head
         # def getNumbers(node):
         #     nums = []
         #     cur = node
@@ -61,6 +129,11 @@ class Solution:
         #     carry = sum_ // 10
 
         # return head
+            
+            
+
+
+        
         
 
 
--- hmm.py
+++ hmm.py
@@ -1,3 +1,72 @@
+# important module
+import math
+
+def doTheyBelong(x1, y1, x2, y2, x3, y3, xp, yp, xq, yq):
+    # area of triangle
+    def area(x1, y1, x2, y2, x3, y3):
+        return abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0)
+
+    # function isInside to check the point
+    def isInside(x1, y1, x2, y2, x3, y3, p1, p2):
+        # Calculate area of triangle ABC
+        A = area(x1, y1, x2, y2, x3, y3)
+
+        # Calculate area of triangle PBC
+        A1 = area(x2, y2, x3, y3, p1, p2)
+
+        # Calculate area of triangle PAC
+        A2 = area(x1, y1, x3, y3, p1, p2)
+
+        # Calculate area of triangle PAB
+        A3 = area(x2, y2, x1, y1, p1, p2)
+
+        # Check if sum of A1, A2, and A3 is same as A
+        return A == A1 + A2 + A3
+
+    def length(x1, y1, x2, y2):
+        distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        return distance
+
+
+    ab = length(x1, y1, x2, y2)
+    bc = length(x2, y2, x3, y3)
+    ac = length(x1, y1, x3, y3)
+
+
+    if not (ab + bc > ac and bc + ac > ab and ab + ac > bc):
+        return 0
+    elif isInside(x1, y1, x2, y2, x3, y3, xp, yp,) and isInside(x1, y1, x2, y2, x3, y3, xq, yq,):
+        return 3
+    elif isInside(x1, y1, x2, y2, x3, y3, xp, yp,):
+        return 1
+    elif isInside(x1, y1, x2, y2, x3, y3, xq, yq,):
+        return 2
+    else:
+        return 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # import sys
 # import collections
 
@@ -36,20 +105,20 @@
 
 # print(solution([911,1,3,1000,1000,2,2,999,1000,911]))
 
-import numpy as np
+# import numpy as np
 
-# Percentage data for black faculty
-faculty_percentages = [2.5, 2.5, 2.7, 2.6, 2.5, 2.5, 2.3, 3.1, 2.4, 2.5, 2.5]
+# # Percentage data for black faculty
+# faculty_percentages = [2.5, 2.5, 2.7, 2.6, 2.5, 2.5, 2.3, 3.1, 2.4, 2.5, 2.5]
 
-# Percentage data for black individuals who graduate
-graduation_percentages = [4.7, 5.1, 4.9, 4.7, 4.7, 4.4, 4.7, 4.7, 4.8, 4.8, 5.0]
+# # Percentage data for black individuals who graduate
+# graduation_percentages = [4.7, 5.1, 4.9, 4.7, 4.7, 4.4, 4.7, 4.7, 4.8, 4.8, 5.0]
 
-# Reshape the arrays as 2D arrays
-faculty_percentages = np.array(faculty_percentages).reshape(-1, 1)
-graduation_percentages = np.array(graduation_percentages).reshape(-1, 1)
+# # Reshape the arrays as 2D arrays
+# faculty_percentages = np.array(faculty_percentages).reshape(-1, 1)
+# graduation_percentages = np.array(graduation_percentages).reshape(-1, 1)
 
-# Calculate the correlation coefficient
-correlation_coefficient = np.corrcoef(faculty_percentages[:, 0], graduation_percentages[:, 0])[0, 1]
+# # Calculate the correlation coefficient
+# correlation_coefficient = np.corrcoef(faculty_percentages[:, 0], graduation_percentages[:, 0])[0, 1]
 
-print("Correlation coefficient:", correlation_coefficient)
+# print("Correlation coefficient:", correlation_coefficient)
 
