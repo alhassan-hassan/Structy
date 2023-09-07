c573b1c5092ffa2f6b769834e7e18139c86d43b9
alhassan-hassan <alhassanhassan360@gmail.com>
2023-09-07 01:38:45 +0000

    refresher

--- bloomberg/min_rem_valid_parenthesis.py
+++ bloomberg/min_rem_valid_parenthesis.py
@@ -29,3 +29,32 @@ class Solution:
 
         print(stack)
         return "".join(stack)
+    
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        rem = set()
+
+        for i, c in enumerate(s):
+            if c not in "()":
+                continue
+
+            if c == "(":
+                stack.append(i)
+            elif not stack:
+                rem.add(i)
+            else:
+                stack.pop()
+
+        rem = rem.union(set(stack))
+        s_builder = []
+
+        for ind, ch in enumerate(s):
+            if ind not in rem:
+                s_builder.append(ch)
+
+        return "".join(s_builder)
+
+
+
+        
