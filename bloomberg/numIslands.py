7dfc534beee3aadf7bd09452ed05a7386e0ba7d1
alhassan-hassan <alhassanhassan360@gmail.com>
2023-09-07 14:00:24 +0000

    refresher

--- bloomberg/numIslands.py
+++ bloomberg/numIslands.py
@@ -38,42 +38,38 @@ class Solution:
 
         return islands
     
-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
 
-        verts = defaultdict(list)
-        queue = deque([(root, 0)])
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
 
-        min_ = max_ = 0
+        def bfs(r, c):
+            queue = deque([(r, c)])
+            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-        while queue:
-            node, ind = queue.popleft()
-            if node is not None:
-                verts[ind].append(node.val)
+            while queue:
+                rr, cc = queue.popleft()
+                for rd, cd in directions:
+                    r, c = rr + rd, cc + cd
 
-                queue.append((node.left, ind - 1))
-                queue.append((node.right, ind + 1))
+                    if r in range (rows) and c in range(cols) and (r, c) not in visited and grid[r][c] == "1":
+                        queue.append((r, c))
+                        visited.add((r, c))
+                
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
+                    islands += 1
 
-                min_ = min(min_, ind)
-                max_ = max(max_, ind)
+        return islands
 
-        output = []
-
-        for key in range(min_, max_ + 1):
-            output.append(verts[key])
-
-        return output
 
 """
-COMPLEXITIES: TIME: O(N)
-              SPACE:O(N)
+COMPLEXITIES: TIME : O(N * M)
+              SPACE: -------
 """
