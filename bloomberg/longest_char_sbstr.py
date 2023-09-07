e69f765e4ddbb65f9b5cf56886d39dde5a71f40f
alhassan-hassan <alhassanhassan360@gmail.com>
2023-09-07 21:59:56 +0000

    refresher

--- bloomberg/longest_char_sbstr.py
+++ bloomberg/longest_char_sbstr.py
@@ -13,3 +13,22 @@ class Solution:
             count = max(count, r - l + 1)
 
         return count
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        track = set()
+        count = lo = 0
+
+        for r in range(len(s)):
+            while s[r] in track:
+                track.remove(s[lo])
+                lo += 1
+
+            track.add(s[r])
+
+            count = max(count, r - lo + 1)
+
+        return count
+
+"BOTH TIME AND SPACE IS O(N)"
