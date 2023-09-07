69998e04ab6fd81fb9484e130f416c6d4a95868d
alhassan-hassan <alhassanhassan360@gmail.com>
2023-09-07 13:53:54 +0000

    refresher

--- bloomberg/sort_nums_power_val.py
+++ bloomberg/sort_nums_power_val.py
@@ -27,3 +27,26 @@ class Solution:
         
         final.sort(key = lambda x: x[0] )
         return final[k-1][1]
+    
+
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        mem = {}
+        def power(num):
+            if num == 1:
+                return 0 
+            if num in mem:
+                return mem[num]
+
+            if num % 2 == 0:
+                mem[num] = 1 + power(num // 2)
+            else:
+                mem[num] = 1 + power(3 * num + 1)
+
+            return mem[num]
+
+        res = [(i, power(i)) for i in range(lo, hi + 1)]
+        res.sort(key=lambda x: (x[1], x[0]))
+
+        return res[k - 1][0]
+
