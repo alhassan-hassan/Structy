45fea75015bffd56fbd49426b9109d7daccd98b3
alhassan-hassan <alhassanhassan360@gmail.com>
2023-09-25 03:05:18 +0000

    refresher

--- bloomberg/check-if-one-string-swap-can-make-strings-equal.py
+++ bloomberg/check-if-one-string-swap-can-make-strings-equal.py
@@ -10,7 +10,7 @@ class Solution:
             if s1[i] != s2[i]:
                 diff.append([s1[i], s2[i]])
 
-        if len(diff) == 2 and diff[0] == diff[-1][::-1]:
+        if len(diff) == 2 and diff[0] == diff[-1][::-1]: # or if len(diff) == 2 and diff[0][0] == diff[-1][1] and diff[0][1] == diff[-1][0]:
             return True
         return False
         """
