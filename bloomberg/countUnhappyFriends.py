dd7ffbbfaafa5dd865d7b9b95f9353a29a41e7b8
alhassan-hassan <alhassanhassan360@gmail.com>
2024-11-07 20:59:28 +0000

    decoding the string

--- bloomberg/countUnhappyFriends.py
+++ bloomberg/countUnhappyFriends.py
@@ -1,24 +1,71 @@
+from typing import List, Dict
+
 class Solution:
     def unhappyFriends(self, n: int, preferences: List[List[int]], pairs: List[List[int]]) -> int:
         pref = {}
 
-        # GET EACH PERSON AND THE PERSON(S) THEY PREFERE MORE THAN THE ONE THEY ARE PAIRED WITH
+        # TRACK ALL UNHAPPY PREFERENCES
         for x, y in pairs:
             pref[x] = set(preferences[x][0 : preferences[x].index(y)])
             pref[y] = set(preferences[y][0 : preferences[y].index(x)])
 
-        count = 0
+        unhappyFriends = 0
 
-        # SOMEONE ISN'T HAPPY IF THEY PREFERE SOMEONE TO THE PERSON THEY ARE PAIRED WITH.
-        # AND THAT PERSON ALSO PREFERS THEM TO THE PERSON THEY ARE ALSO PAIRED WITH
         for x in pref:
             for y in pref[x]:
                 if x in pref[y]:
-                    # if one is found, we move to the next person (break)
-                    count += 1
+                    unhappyFriends += 1
                     break
+        return unhappyFriends
+
+
+    #     # Create partner lookup dictionary
+    #     partner = {}
+    #     for x, y in pairs:
+    #         partner[x] = y
+    #         partner[y] = x
+
+    #     # Create a ranking dictionary for quick preference comparisons
+    #     ranking = {i: {} for i in range(n)}
+    #     for i in range(n):
+    #         for rank, friend in enumerate(preferences[i]):
+    #             ranking[i][friend] = rank
+
+    #     # Count unhappy friends
+    #     unhappy_count = 0
+    #     for x in range(n):
+    #         y = partner[x]
+    #         for u in preferences[x]:
+    #             # If x prefers u over their current partner y
+    #             if ranking[x][u] < ranking[x][y]:
+    #                 v = partner[u]
+    #                 # And u prefers x over their current partner v
+    #                 if ranking[u][x] < ranking[u][v]:
+    #                     unhappy_count += 1
+    #                     break  # Stop further checks once x is confirmed unhappy
+
+    #     return unhappy_count
+
+    # def unhappyFriends(self, n: int, preferences: List[List[int]], pairs: List[List[int]]) -> int:
+    #     pref = {}
+
+    #     # GET EACH PERSON AND THE PERSON(S) THEY PREFERE MORE THAN THE ONE THEY ARE PAIRED WITH
+    #     for x, y in pairs:
+    #         pref[x] = set(preferences[x][0 : preferences[x].index(y)])
+    #         pref[y] = set(preferences[y][0 : preferences[y].index(x)])
+
+    #     count = 0
+
+    #     # SOMEONE ISN'T HAPPY IF THEY PREFERE SOMEONE TO THE PERSON THEY ARE PAIRED WITH.
+    #     # AND THAT PERSON ALSO PREFERS THEM TO THE PERSON THEY ARE ALSO PAIRED WITH
+    #     for x in pref:
+    #         for y in pref[x]:
+    #             if x in pref[y]:
+    #                 # if one is found, we move to the next person (break)
+    #                 count += 1
+    #                 break
  
-        return count
+    #     return count
         
 
 """
--- bloomberg/decode_String.py
+++ bloomberg/decode_String.py
@@ -44,3 +44,49 @@ class Solution:
                 stack.append(int(count) * substr)
 
         return "".join(stack)
+    
+
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        
+        for char in s:
+            if char != ']':
+                stack.append(char)
+            else:
+                substr = ""
+                while stack and stack[-1] != "[":
+                    substr = stack.pop() + substr
+                
+                stack.pop()
+
+                count = ""
+                while stack and stack[-1].isnumeric():
+                    count = stack.pop() + count
+                
+                stack.append(int(count) * substr)
+
+        return "".join(stack)
+
+
+
+        # stack = []
+
+        # for i in range(len(s)):
+        #     if s[i] != "]":
+        #         stack.append(s[i])
+        #     else:
+        #         substr = ""
+        #         while stack[-1] != "[":
+        #             substr = stack.pop() + substr
+        #         stack.pop()
+
+        #         count = ""
+
+        #         while stack and stack[-1].isnumeric():
+        #             count = stack.pop() + count
+
+        #         stack.append(int(count) * substr)
+
+        # return "".join(stack)
