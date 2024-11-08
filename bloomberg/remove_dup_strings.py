7fb7ec8438220aa050b8aa0d736dec7aa4fefc1c
alhassan-hassan <alhassanhassan360@gmail.com>
2024-11-08 22:38:25 +0000

    remove adjacent duplicates

--- bloomberg/remove_dup_strings.py
+++ bloomberg/remove_dup_strings.py
@@ -37,4 +37,27 @@ class Solution:
         # return tempt
                 
                 
-            
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = []
+        for char in s:
+            if stack and stack[-1][0] == char:
+                stack[-1][1] += 1
+
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append([char, 1])
+
+        return "".join(char * count for char, count in stack)
+
+        # tempt = []
+        # for i in range(len(s)):
+        #     if tempt and s[i] == tempt[-1][0]:
+        #         tempt[-1][1] += 1
+        #         if tempt[-1][1] == k:
+        #             tempt.pop()
+        #     else:
+        #         tempt.append([s[i], 1])
+                    
+        # return ''.join(key * val for key, val in tempt)
