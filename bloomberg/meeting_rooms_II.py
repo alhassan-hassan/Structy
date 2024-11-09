39e6580fb706e968a9eea1c1dc53c3da8be6f8c0
alhassan-hassan <alhassanhassan360@gmail.com>
2024-11-09 13:19:12 +0000

    meeting rooms

--- bloomberg/meeting_rooms_II.py
+++ bloomberg/meeting_rooms_II.py
@@ -1,19 +1,40 @@
+import heapq
 class Solution:
-    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        start = sorted([x[0] for x in intervals])
-        end = sorted([x[1] for x in intervals])
+    def minMeetingRooms(intervals):
+        heap = []
+        intervals.sort(key = lambda x: x[0])
 
-        p1 = p2 = 0
-        count = res = 0
+        for interval in intervals:
+            start, end = interval
+            if heap and start >= heap[0]:
+                heapq.heappop(heap)
 
-        while p1 < len(start):
-            if start[p1] < end[p2]:
-                count += 1
-                p1 += 1
-            else:
-                p2 += 1
-                count -= 1
+            heapq.heappush(heap, end)
 
-            res = max(res, count)
+        return len(heap)
+    
+# run the solution code
+# intervals = [[0, 30], [5, 10], [15, 20]]
+intervals = [[7, 10], [2, 4]]
+test = Solution.minMeetingRooms(intervals)
+print(test)
 
-        return res
+            
+
+        # start = sorted([x[0] for x in intervals])
+        # end = sorted([x[1] for x in intervals])
+
+        # p1 = p2 = 0
+        # count = res = 0
+
+        # while p1 < len(start):
+        #     if start[p1] < end[p2]:
+        #         count += 1
+        #         p1 += 1
+        #     else:
+        #         p2 += 1
+        #         count -= 1
+
+        #     res = max(res, count)
+
+        # return res
