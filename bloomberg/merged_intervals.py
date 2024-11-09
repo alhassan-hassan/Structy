f4e02b01a2881bf74ed2d14bf78fcd660b14f0f9
alhassan-hassan <alhassanhassan360@gmail.com>
2024-11-09 14:52:45 +0000

    merge intervals

--- bloomberg/merged_intervals.py
+++ bloomberg/merged_intervals.py
@@ -10,3 +10,32 @@ class Solution:
                 res[-1][1] = max(res[-1][1], intervals[i][1])
 
         return res
+    
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda x: x[0])
+        stack = []
+
+        for interval in intervals:
+            if not stack or interval[0] > stack[-1][1]:
+                stack.append(interval)
+            else:
+                stack[-1][1] = max(stack[-1][1], interval[1])
+        return stack
+
+        # if not intervals:
+        #     return intervals
+
+        # intervals.sort()
+        # res = [intervals[0]]
+
+        # for i in range(len(intervals)):
+        #     last = res[-1]
+        #     if last[1] >= intervals[i][0]:
+        #         res[-1][1] = max(last[1], intervals[i][1])
+
+        #     else:
+        #         res.append(intervals[i])
+
+        # return res
+        
