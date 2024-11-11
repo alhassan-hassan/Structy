ff2ff08bebbf197b849e5ca19546c12532c9838d
alhassan-hassan <alhassanhassan360@gmail.com>
2024-11-11 23:19:09 +0000

    add two numbers II

--- bloomberg/add_two_numbersII.py
+++ bloomberg/add_two_numbersII.py
@@ -1,58 +1,34 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-
-    # OPTION 1
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def summer(node):
-            sum_ = 0
+        def getNums(node):
+            if not node:
+                return []
+
             cur = node
+            stack = []
 
             while cur:
-                sum_ = sum_ * 10 + cur.val
+                stack.append(cur.val)
                 cur = cur.next
-            return sum_
+            return stack
 
-        num1 = summer(l1)
-        num2 = summer(l2)
+        stack1 = getNums(l1)
+        stack2 = getNums(l2)
 
-        total = num1 + num2
-
-        dummy = ListNode(0)
-        cur = dummy
-
-        for num in str(total):
-            cur.next = ListNode(num)
-            cur = cur.next
-
-        return dummy.next
-
-
-        # OPTION 2
-        
-        def getNumbers(node):
-            nums = []
-            cur = node
-
-            while cur:
-                nums.append(cur.val)
-                cur = cur.next
-
-            return nums
-
-        list1 = getNumbers(l1)
-        list2 = getNumbers(l2)
-
-        carry = 0
         head = None
+        carry = 0
 
-        while list1 or list2 or carry != 0:
+        while stack1 or stack2 or carry != 0:
             sum_ = carry
-            if list1: sum_ += list1.pop()
-            if list2: sum_ += list2.pop()
+            if stack1:
+                sum_ += stack1.pop()
+            if stack2:
+                sum_ += stack2.pop()
 
             newNode = ListNode(sum_ % 10)
             newNode.next = head
@@ -61,14 +37,20 @@ class Solution:
             carry = sum_ // 10
 
         return head
-    
-        # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         def nums(head):     
             cur = head
             res = []
@@ -134,7 +116,3 @@ class Solution:
 
 
         
-        
-
-
-
