c94f4cf3d7540ff8cd0e2cb19f6f5f4d0a02701c
alhassan-hassan <alhassanhassan360@gmail.com>
2024-12-03 00:44:50 +0000

    let's meet

--- bloomberg/numIslands.py
+++ bloomberg/numIslands.py
@@ -1,78 +1,35 @@
-import collections
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-
-        rows, cols = len(grid), len(grid[0])
-        visited = set()
-        islands = 0
-
-        def explore_adjacebt_islands(r, c):
-            q = collections.deque()
-            q.append((r, c))
-            visited.add((r,c))
-
-            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
-            while q:
-                row, col = q.popleft()
-
-                for rd, cd in directions:
-                    cr = row + rd
-                    cc = col + cd
-
-                    if (cr in range(rows) and 
-                        cc in range(cols) and 
-                        grid[cr][cc] == "1" and 
-                        (cr, cc) not in visited):
-
-                        q.append((cr, cc))
-                        visited.add((cr, cc))
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    explore_adjacebt_islands(r, c)
-                    islands += 1
-
-        return islands
-    
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-
-        rows, cols = len(grid), len(grid[0])
-        visited = set()
+            
+        ROWS = len(grid)
+        COLS = len(grid[0])
         islands = 0
 
         def bfs(r, c):
-            queue = deque([(r, c)])
             directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            dq = deque([(r,c)])
+            grid[r][c] = "0"
 
-            while queue:
-                rr, cc = queue.popleft()
+            while dq:
+                rr, cc = dq.popleft()
                 for rd, cd in directions:
-                    r, c = rr + rd, cc + cd
+                    nr = rr + rd
+                    nc = cc + cd
 
-                    if r in range (rows) and c in range(cols) and (r, c) not in visited and grid[r][c] == "1":
-                        queue.append((r, c))
-                        visited.add((r, c))
-                
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    bfs(r, c)
+                    if nr in range(ROWS) and nc in range(COLS) and grid[nr][nc] == "1":
+                        grid[nr][nc] = "0"
+                        dq.append((nr,nc))
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == "1":
                     islands += 1
+                    bfs(i, j)
 
         return islands
 
-
-"""
-COMPLEXITIES: TIME : O(N * M)
-              SPACE: -------
-"""
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
@@ -101,8 +58,8 @@ class Solution:
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == "1" and (i, j) not in seen:
-                    bfs(i, j)
                     islands += 1
+                    bfs(i, j)
 
         return islands
 
